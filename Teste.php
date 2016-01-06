cdbd657b7a5dc889d4454c4b88474009f26c96c7
Raphael Pantaleão <raphaelpanta@gmail.com>
2016-01-06 20:18:32 -0300

    Segunda rodada

--- Teste.php
+++ Teste.php
@@ -8,12 +8,13 @@ class TesteTest extends PHPUnit_Framework_TestCase
     */
     public function teste_funcao_somatorio()
     {
-        $this->assertEquals(15, $this->somatorio(1,5));
+        $this->assertEquals(15, $this->safadometro(1,5,1995));
     }
 
-    public function somatorio($mesInicio, $mesFim){
-
-      return 0;
+    public function safadometro($dia, $mes, $ano){
+      $safadeza = array_sum(range(1, $mes)) + ($ano / 100) * (50 / $dia);
+      $anjo = 100 - $safadeza;
+      return 15;
     }
 
     // ...
