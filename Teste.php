bd957e7cddb1332cb4ffbe602fbfb07c108fdf99
Raphael Pantaleão <raphaelpanta@gmail.com>
2016-01-06 20:11:13 -0300

    Primeira rodada

---
+++ Teste.php
@@ -0,0 +1,20 @@
+<?php
+class TesteTest extends PHPUnit_Framework_TestCase
+{
+
+
+    /**
+    * @Test
+    */
+    public function teste_funcao_somatorio()
+    {
+        $this->assertEquals(15, $this->somatorio(1,5));
+    }
+
+    public function somatorio($mesInicio, $mesFim){
+
+      return 0;
+    }
+
+    // ...
+}
