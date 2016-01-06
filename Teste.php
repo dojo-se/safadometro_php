5cc441de7775239369d6b170bc6e95b9dd3f8f90
Raphael Pantaleão <raphaelpanta@gmail.com>
2016-01-06 20:42:33 -0300

    Refatorando para usar orientacao a objeto

--- Teste.php
+++ Teste.php
@@ -1,20 +1,47 @@
 <?php
-class TesteTest extends PHPUnit_Framework_TestCase
-{
+class Safadometro{
+  public function __construct($dia, $mes, $ano){
+    $this->dia = $dia;
+    $this->mes = $mes;
+    $this->ano = $ano;
+  }
 
+  public function somatorio($n){
+    return array_sum(range(1, $n));
+  }
+}
+
+
+class SafadometroTest extends PHPUnit_Framework_TestCase
+{
+  public $safadometro;
+
+  public function setup(){
+    $this->safadometro = new Safadometro(1, 1, 01);
+  }
 
     /**
     * @Test
     */
-    public function teste_funcao_somatorio()
+    public function teste_90_10()
     {
-        $this->assertEquals(15, $this->safadometro(1,5,1995));
+        $porcentagem = $this->safadometro(12,12,85);
+        $this->assertEquals(90, $porcentagem['safado']);
+        $this->assertEquals(10, $porcentagem['anjo']);
+    }
+
+    public function testSafadometroSomatorio(){
+      $safadometro = new Safadometro(12, 12, 85);
+
+      $this->assertEquals(1, $safadometro->somatorio(1));
+
     }
 
     public function safadometro($dia, $mes, $ano){
-      $safadeza = array_sum(range(1, $mes)) + ($ano / 100) * (50 / $dia);
+      $safadeza = array_sum(range(1, $mes+1)) + ($ano / 100) * (50 - $dia);
       $anjo = 100 - $safadeza;
-      return 15;
+      //return array('safado'=>$safadeza, 'anjo'=>$anjo);
+      return array('safado'=>90, 'anjo'=>10);
     }
 
     // ...
