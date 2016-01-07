6986de9321d8b67c8c364755acab251d68f2bf9f
Raphael Pantaleão <raphaelpanta@gmail.com>
2016-01-06 21:06:08 -0300

    Safadômetro orientado a objetos

--- Teste.php
+++ Teste.php
@@ -9,6 +9,13 @@ class Safadometro{
   public function somatorio($n){
     return array_sum(range(1, $n));
   }
+
+  public function getSafadeza() {
+    $safadeza = round(array_sum(range(1, $this->mes)) + ($this->ano / 100) * (50 - $this->dia));
+    $anjo = 100 - $safadeza;
+
+    return array('safado'=>$safadeza, 'anjo'=>$anjo);
+  }
 }
 
 
@@ -17,17 +24,49 @@ class SafadometroTest extends PHPUnit_Framework_TestCase
   public $safadometro;
 
   public function setup(){
-    $this->safadometro = new Safadometro(1, 1, 01);
+    $this->safadometro = new Safadometro(1, 1, 1);
   }
 
     /**
     * @Test
     */
-    public function teste_90_10()
+    public function teste_02_98()
     {
-        $porcentagem = $this->safadometro(12,12,85);
-        $this->assertEquals(90, $porcentagem['safado']);
-        $this->assertEquals(10, $porcentagem['anjo']);
+        $porcentagem = $this->safadometro(1,1,3);
+        $this->assertEquals(2, $porcentagem['safado']);
+        $this->assertEquals(98, $porcentagem['anjo']);
+    }
+
+    /**
+    * @Test
+    */
+    public function teste_01_99()
+    {
+        $porcentagem = $this->safadometro(1,1,1);
+        $this->assertEquals(1, $porcentagem['safado']);
+        $this->assertEquals(99, $porcentagem['anjo']);
+    }
+
+    /**
+    * @Test
+    */
+    public function teste_02_98_classe()
+    {
+      $safadometro = new Safadometro(1, 1, 3);
+      $porcentagem = $safadometro->getSafadeza();
+      $this->assertEquals(2, $porcentagem['safado']);
+      $this->assertEquals(98, $porcentagem['anjo']);
+    }
+
+    /**
+    * @Test
+    */
+    public function teste_01_99_classe()
+    {
+      $safadometro = new Safadometro(1, 1, 1);
+      $porcentagem = $safadometro->getSafadeza();
+      $this->assertEquals(1, $porcentagem['safado']);
+      $this->assertEquals(99, $porcentagem['anjo']);
     }
 
     public function testSafadometroSomatorio(){
@@ -38,11 +77,11 @@ class SafadometroTest extends PHPUnit_Framework_TestCase
     }
 
     public function safadometro($dia, $mes, $ano){
-      $safadeza = array_sum(range(1, $mes+1)) + ($ano / 100) * (50 - $dia);
+      $safadeza = round(array_sum(range(1, $mes)) + ($ano / 100) * (50 - $dia));
+      //echo $safadeza;
       $anjo = 100 - $safadeza;
-      //return array('safado'=>$safadeza, 'anjo'=>$anjo);
-      return array('safado'=>90, 'anjo'=>10);
+      return array('safado'=>$safadeza, 'anjo'=>$anjo);
+      //return array('safado'=>90, 'anjo'=>10);
     }
 
-    // ...
 }
